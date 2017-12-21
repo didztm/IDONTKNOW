9d2bda9ec4c2060c3a449b517a3795a50572e4af
Technifutur <barudy.diego@gmail.com>
2017-12-21 12:18:19 +0100

    first commit

---
+++ Assets/Scripts/AndroidExplore.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+	#region Private and Protected Members
+
+    #endregion
+    #region Public Members
+
+    #endregion
+
+    #region Public void
+
+    #endregion
+
+    #region System
+
+    void Start ()
+    {
+        
+    }
+
+    void Update ()
+    {
+        	
+    }
+
+    #endregion
+
+    #region Tools Debug and Utility
+
+    #endregion
+
+    
+}
